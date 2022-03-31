37d4a26431efd1d2e56cb1f4110fc42956c96b1f
Rizk Gendy <80922036+rizk-gendy@users.noreply.github.com>
2022-03-31 14:43:35 +0200

    SignUp Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Featured from "./components/Featured";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
     <Featured/>
+    <SignUp/>
     </>
   );
 }
